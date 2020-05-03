0923d4e84aff59a7c0af6b6dd8bcb42e8dde4ed6
cabrody <crb99@aol.com>
2020-05-03 13:57:26 -0700

    Add files via upload

--- code/spotipy_test.py
+++ code/spotipy_test.py
@@ -1,6 +1,7 @@
 import sys
 import spotipy
 import spotipy.util as util
+import urllib.request
 
 
 
@@ -16,7 +17,21 @@ if __name__ == '__main__':
 
     if token:
         sp = spotipy.Spotify(auth=token)
-        track_to_analyze = sp.audio_analysis("spotify:track:1i6N76fftMZhijOzFQ5ZtL")
-        print(track_to_analyze['sections'])
+        track_to_analyze = sp.audio_features("spotify:track:1i6N76fftMZhijOzFQ5ZtL")
+        #track_sections = track_to_analyze['sections']
+        #song_tempos = []
+        #song_keys = []
+        #song_time_signature = []
+        #song_loudness = []
+        #for s in track_sections:
+        #    song_tempos.append(s["tempo"])
+        #    song_time_signature.append(s["time_signature"])
+        #    song_loudness.append(s["loudness"])
+
+        print(sp.track("spotify:track:1i6N76fftMZhijOzFQ5ZtL")['preview_url'])
+        #downloads 30 second clip of requested file at local path
+        urllib.request.urlretrieve(sp.track("spotify:track:1i6N76fftMZhijOzFQ5ZtL")['preview_url'], 'C:/Users/camer/Downloads/test2')
+
+
     else:
         print("Can't get token for", username)
